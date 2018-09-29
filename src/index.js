5cc0eeded8ed631f63f5142c3dfdb2100d244fdd
vetal11 <vetalblack@list.ru>
2018-09-29 23:19:09 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+	let stack = [];
+	for(let i = 0; i<str.length; i++){
+	  	for(let j = 0; j<bracketsConfig.length; j++){
+	  		if (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+	  			if (stack[stack.length-1] === str[i]){
+	  					stack.pop();
+	  			} else{
+	  				stack.push(str[i]);
+	  			} 
+	  		}
+	  		else if (str[i] === bracketsConfig[j][0]) {
+	  			stack.push(j+1);
+	  		}
+	  		else if (str[i] === bracketsConfig[j][1]){
+	  			if ((stack[stack.length-1] - (j+1)) === 0){
+	  				stack.pop();
+	  			} 
+	  			else {
+	  				return false;
+	  			}
+	  		}
+	  	}
+	}
+	return stack.length === 0 ? true : false;
 }
